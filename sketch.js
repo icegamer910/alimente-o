be1098ae95ee0059ffe48dfba977c75219ab0b7f
icegamer910 <101666859+icegamer910@users.noreply.github.com>
2022-06-09 15:56:52 -0300

    9/6

--- sketch.js
+++ sketch.js
@@ -9,7 +9,21 @@ const Composite = Matter.Composite;
 
 let engine;
 let world;
-var terra
+var terra;
+var acorda;
+var papinha;
+var cartilagem;
+var salinhadajanta;
+var comidinha;
+var eunahoradajanta;
+var alfredo;
+var desaparecedor;
+
+function preload(){
+  salinhadajanta = loadImage("./imagem falando/background.png");
+  comidinha = loadImage("./imagem falando/cenoura.webp");
+  eunahoradajanta = loadImage("./imagem falando/Rabbit-01.png");
+}
 
 function setup() 
 {
@@ -19,17 +33,37 @@ function setup()
  
   rectMode(CENTER);
   ellipseMode(RADIUS);
+  imageMode(CENTER);
   textSize(50)
   terra = new Terra(200,690,600,20);
+  acorda = new Rope(6, {x: 245, y: 35});
+
+  papinha = Bodies.rectangle(300,300,15,50);
+  World.add(world,papinha);
+  cartilagem = new Cartilagem(acorda, papinha);
+  alfredo = createSprite(250,650,100,100);
+alfredo.addImage(eunahoradajanta);
+alfredo.scale = 0.2
+desaparecedor = createImg("./imagem falando/cut_btn.png");
+desaparecedor.position(220,30);
+desaparecedor.size(75,75);
+desaparecedor.mouseClicked(barrigacheia);
+
 }
 
 function draw() 
 {
   background(51);
+  image(salinhadajanta, 250, 350, 500, 700);
   Engine.update(engine);
    terra.artista();
+   acorda.show();
+   image(comidinha,papinha.position.x, papinha.position.y, 50, 80);
+   drawSprites()
 }
 
-
-
-
+function barrigacheia(){
+  acorda.break();
+  cartilagem.sumiu();
+  cartilagem = null
+}
